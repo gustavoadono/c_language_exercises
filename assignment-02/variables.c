3bed7b7ba20bf7bfcbd9ea2a8b90a4c4412d800d
Gustavo Adono <adonogustavo@gmail.com>
2023-03-29 20:06:13 -0300

    [added assignment 02 solution]

---
+++ assignment-02/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+  int a = 5;
+  float b = 3.14;
+  char c[] = "Hello, World! is a char!";
+
+  printf("%d is an integer!\n",a);
+  printf("%f is a float\n",b);
+  printf("%s\n",c);
+
+  return 0;
+}
