fdda36da1e724936ca6cb526aa3e2e6ada3b9c75
Gustavo Adono <adonogustavo@gmail.com>
2023-03-30 22:51:44 -0300

    [added assignment 03 extra soluition]

---
+++ assignment-03/welcome_extra.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  //initialize vars
+  char *first;
+  char *last;
+
+  //prompt user to input first and last name and use scanf() to store those to the initiliazed vars
+  printf("Enter your first name: ");
+  scanf("%ms", &first);
+  printf("Enter your last name: ");
+  scanf("%ms", &last);
+
+  //print the welcome message!
+  printf("Hello %s %s!\n", first, last);
+
+  free(first);
+  free(last);
+
+  // here we can see that, the value no long exist.
+  printf("Hello %s %s!\n", first, last);
+  
+}
+
