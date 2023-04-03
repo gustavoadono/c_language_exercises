d94fc49596e087651543196f1f96129faccf90ba
Gustavo Adono <adonogustavo@gmail.com>
2023-04-02 23:59:49 -0300

    [added assignment 07 solution]

---
+++ assignment-07/quadratic.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (int argc, char *argv[])
+{
+  float a,b,c,x1,x2,delta;
+
+  printf("Enter the value of variable 'A':\n");
+  scanf("%f",a);
+
+  printf("Enter the value of variable 'B':\n");
+  scanf("%f",b);
+  
+  printf("Enter the value of variable 'C':\n");
+  scanf("%f",c);
+  
+  delta = b*b-(4*a*c);
+  delta = sqrt(delta);
+
+  x1 =  ((-b) + delta/2*a);
+  
+  x2 =  ((-b) - delta/2*a);
+
+  printf("The solution using the '+' operator is: %f\n",x1);
+
+
+  printf("The solution using the '-' operator is: %f\n",x2);
+
+  return 0;
+}
